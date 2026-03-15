09aa053d65b4d7ebbca1000efa69a1c963918ced
TonmoySarker-BD <tonmoycsebd+git@gmail.com>
2026-03-15 11:01:18 +0600

    fix the deployment to the vercel

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import app from "./app";
 import config from "./config";
 
-
 // Start the server
 app.listen(config.port, () => {
   console.log(`Vehicle Rental System listening on port ${config.port}`);
